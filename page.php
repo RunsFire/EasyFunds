13507198c0cf71432e36c903682d07772853fd6e
Em-Xu <154454422+emilie-exu-95@users.noreply.github.com>
2024-10-22 14:06:36 +0200

    Update page.php
    Mise à jour, début de "po-transactions"

--- page.php
+++ page.php
@@ -1,18 +1,21 @@
 <!DOCTYPE html>
-<?php
-	session_start ();
-    // if($_SESSION['typeu']!=2 ||  !isset($_SESSION['login']) && !isset($_SESSION['mdp'])) {
-	// 	header('location:login.php');
-	// }
-?>
+
 <html>
+
+    <?php
+    session_start ();
+        // if($_SESSION['typeu']!=2 ||  !isset($_SESSION['login']) && !isset($_SESSION['mdp'])) {
+        // 	header('location:login.php');
+        // }
+    ?>
+
+
     <head>
         <link rel="stylesheet" href="page.css">
         <meta charset="utf-8">
-        <title>Accueil</title>
+        <title>PO | Trésorerie</title>
+        <link rel="icon" href="easyfunds-icon.png">
         <script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.9.3/html2pdf.bundle.min.js"></script>
-        <link rel="icon" type="image/png" href="easyfunds-icon.png">
-        <script src="exports.js"></script>
     </head>
 
     <!-- HEADER -->
@@ -46,12 +49,12 @@
             <div class="frame options">
                 
                 <!-- TRÉSORERIE DES COMPTES CLIENTS -->
-                <input href=#po-tous-clients type="radio" class="option-radio" name="option" id="tous-clients" checked>
-                <label for="tous-clients" class="option-label" onclick="displayTable('tous-clients', 'po-tous-clients')">Trésorerie des comptes clients</label>
+                <input type="radio" class="option-radio" name="option" id="tous-clients" checked>
+                <label for="tous-clients" class="option-label" onclick="displayTable('tous-clients', 'po-tous-clients-display')">Trésorerie des comptes clients</label>
 
                 <!-- TRÉSORERIE PAR COMPTE CLIENT -->
                 <input type="radio" class="option-radio" name="option" id="par-client">
-                <label for="par-client" class="option-label" onclick="displayTable('par-client', 'po-par-client')">Trésorerie par compte client</label>
+                <label for="par-client" class="option-label" onclick="displayTable('par-client', 'po-par-client-display')">Trésorerie par compte client</label>
 
             </div>
         </section>
@@ -60,16 +63,16 @@
         <!-- DISPLAY TABLES + DATAS -->
         <section class="table-display">
 
-            <!-- PO TOUS CLIENTS -->
-            <div id="po-tous-clients" class="display active">
+            <!-- PO TOUS CLIENTS, remplit par défaut -->
+            <div id="po-tous-clients-display" class="display active">
 
                 <!-- FILTRES -->
                 <div class="frame filtres">
-                    <form>
+                    <form action="nomDuFichier.php?display='po-tous-client'" method="get">
                         <input type="text" class="filtre" placeholder="N° SIREN">
                         <input type="text" class="filtre" placeholder="Raison sociale">
                         <input type="date" class="filtre">
-                        <button type="submit" class="search">Rechercher</button>
+                        <button type="submit">Rechercher</button>
                     </form>
                 </div>
 
@@ -90,51 +93,90 @@
                     <!-- TABLEAU DATAS -->
                     <div class="table-datas">
                         <table class="frame">
+                            <!-- TEMPLATE DONNÉES
+                                <tr class="style1">
+                                    <td style="width:20%">N° SIREN</td>
+                                    <td style="width:20%">Raison Sociale</td>
+                                    <td style="width:25%">Nb de transactions</td>
+                                    <td style="width:20%">Date</td>
+                                    <td style="width:20%">Montant total</td>
+                                </tr>
+                                <tr class="style2">
+                                    <td style="width:20%">N° SIREN</td>
+                                    <td style="width:20%">Raison Sociale</td>
+                                    <td style="width:25%">Nb de transactions</td>
+                                    <td style="width:20%">Date</td>
+                                    <td style="width:20%">Montant total</td>
+                                </tr>
+                            -->
                             <tr class="style1">
                                 <td style="width:20%">N° SIREN</td>
                                 <td style="width:20%">Raison Sociale</td>
                                 <td style="width:25%">Nb de transactions</td>
-                                <td style="width:20%">10/10/2024</td>
+                                <td style="width:20%">Date</td>
                                 <td style="width:20%">Montant total</td>
                             </tr>
                             <tr class="style2">
                                 <td style="width:20%">N° SIREN</td>
                                 <td style="width:20%">Raison Sociale</td>
                                 <td style="width:25%">Nb de transactions</td>
-                                <td style="width:20%">10/10/2024</td>
+                                <td style="width:20%">Date</td>
                                 <td style="width:20%">Montant total</td>
                             </tr>
                         </table>
                     </div>
                 </div>
 
-                <!----- EXPORT ----->
-                <form class="frame table-export" onsubmit="return false">
-                    <p>Format du tableau :</p>
-                    <select name="table-format" class="table-format">
-                        <option value="csv">CSV</option>
-                        <option value="pdf">PDF</option>
-                        <option value="xls">XLS</option>
-                    </select>
-                    <button onclik="exportTableToPdf('')" class="export">Exporter</button>
-                </form>
+                <!----- SOUS LE TABLEAU ----->
+                <div class="frame row-space-between" style="margin-top: 2px;">
+
+                    <!-- TRI -->
+                    <form>
+                        <select name="filtre">
+                            <option selected disabled hidden>--</option>
+                            <option value="siren">N° SIREN</option>
+                            <option value="raison">Raison Sociale</option>
+                            <option value="nb-tr">Nb de transactions</option>
+                            <option value="date">Date</option>
+                            <option value="mt-total">Montant total</option>
+                        </select>
+                        <select name="croissance">
+                            <option selected disabled hidden>--</option>
+                            <option value="asc">ordre croissant</option>
+                            <option value="desc">ordre décroissant</option>
+                        </select>
+                        <button type="submit">Trier</button>
+                    </form>
+
+
+                    <!-- EXPORT -->
+                    <form class="table-export" onsubmit="return false">
+                        <p>Format du tableau :</p>
+                        <select name="table-format" class="table-format">
+                            <option value="csv">CSV</option>
+                            <option value="pdf">PDF</option>
+                            <option value="xls">XLS</option>
+                        </select>
+                        <button onclik="exporter('po-tous-clients-display')">Exporter</button>
+                    </form>
+                </div>
 
             </div>
 
-            <!-- PO PAR CLIENT -->
-            <div id="po-par-client" class="display">
+            <!-- PO PAR CLIENT, vide par défaut -->
+            <div id="po-par-client-display" class="display">
                 <!-- FILTRES -->
                 <div class="frame filtres" class="">
-                    <form>
+                    <form action="nomDuFichier.php?display='po-par-client'" method="get">
                         <input type="text" class="filtre" placeholder="N° SIREN du client">
                         <input type="text" class="filtre" placeholder="Raison sociale du client">
                         <input type="date" class="filtre">
-                        <button type="submit" class="search">Rechercher</button>
+                        <button type="submit">Rechercher</button>
                     </form>
                 </div>
 
                 <!----- TABLEAU, headers + datas ----->
-                <div class="table frame">
+                <div class="table frame" id="po-tous-clients">
                     <!-- TABLEAU HEADERS-->
                     <div class="frame table-headers">
                         <table class="frame">
@@ -142,42 +184,109 @@
                                 <th style="width:20%">N° SIREN</th>
                                 <th style="width:20%">Raison Sociale</th>
                                 <th style="width:25%">Nb de transactions</th>
-                                <th style="width:20%">Date</th>
+                                <th style="width:20%">jj/mm/aaaa</th>
                                 <th style="width:20%">Montant total</th>
                             </tr>
                         </table>
                     </div>
                     <!-- TABLEAU DATAS -->
-                    <div class="table-datas">
+                    <div class="table-datas shorter-table" id="po-par-client">
                         <table class="frame">
-                            <tr class="style1">
+                            <!-- TEMPLATE DONNÉES
+
+                                [!] REMPLACER codeRemise par le code de le remise
+
+                                <tr class="style1" onclick="window.location.href='po_transactions.html?remise=codeRemise'">
+                                    <td style="width:20%">N° SIREN</td>
+                                    <td style="width:20%">Raison Sociale</td>
+                                    <td style="width:25%">Nb de transactions</td>
+                                    <td style="width:20%">Date</td>
+                                    <td style="width:20%">Montant total</td>
+                                </tr>
+                                <tr class="style2" onclick="window.location.href='po_transactions.html?remise=codeRemise'">
+                                    <td style="width:20%">N° SIREN</td>
+                                    <td style="width:20%">Raison Sociale</td>
+                                    <td style="width:25%">Nb de transactions</td>
+                                    <td style="width:20%">Date</td>
+                                    <td style="width:20%">Montant total</td>
+                                </tr>
+                            -->
+                            <tr class="style1" onclick="window.location.href='po_transactions.html?remise=codeRemise'">
                                 <td style="width:20%">N° SIREN</td>
                                 <td style="width:20%">Raison Sociale</td>
                                 <td style="width:25%">Nb de transactions</td>
-                                <td style="width:20%">10/10/2024</td>
-                                <td style="width:20%">Montant total</td>
+                                <td style="width:20%">Date</td>
+                                <td style="width:20%">Montant</td>
                             </tr>
-                            <tr class="style2">
+                            <tr class="style2" onclick="window.location.href='po_transactions.html?remise=codeRemise'">
                                 <td style="width:20%">N° SIREN</td>
                                 <td style="width:20%">Raison Sociale</td>
                                 <td style="width:25%">Nb de transactions</td>
-                                <td style="width:20%">10/10/2024</td>
-                                <td style="width:20%">Montant total</td>
+                                <td style="width:20%">Date</td>
+                                <td style="width:20%">Montant</td>
                             </tr>
                         </table>
                     </div>
                 </div>
 
-                <!----- EXPORT ----->
-                <form class="frame table-export" onsubmit="return false">
-                    <p>Format du tableau :</p>
-                    <select name="table-format" class="table-format">
-                        <option value="csv">CSV</option>
-                        <option value="pdf">PDF</option>
-                        <option value="xls">XLS</option>
-                    </select>
-                    <button onclick="exportTableToPdf('po-par-client')" class="export">Exporter</button>
-                </form>
+                <!-- TABLEAU, total -->
+                <div class="frame">
+                    <table class="frame">
+                        <!-- DEFAULT -->
+                        <tr class="end-row">
+                            <td style="width:20%;">-</td>
+                            <td style="width:20%">-</td>
+                            <td style="width:25%">-</td>
+                            <td style="width:20%"></td>
+                            <td style="width:20%">-<td>
+                        </tr>
+                        <!-- Remplissage
+                        <tr class="end-row">
+                            <td style="width:20%;">input N° SIREN</td>
+                            <td style="width:20%">input Raison Sociale</td>
+                            <td style="width:25%">Total de transactions</td>
+                            <td style="width:20%"></td>
+                            <td style="width:20%">Montant total</td>
+                        </tr>
+                        -->
+                        
+                        
+                    </table>
+                </div>
+
+                <!----- SOUS LE TABLEAU ----->
+                <div class="frame row-space-between" style="margin-top: 2px;">
+
+                    <!-- TRI -->
+                    <form>
+                        <select name="filtre">
+                            <option selected disabled hidden>--</option>
+                            <option value="siren">N° SIREN</option>
+                            <option value="raison">Raison Sociale</option>
+                            <option value="nb-tr">Nb de transactions</option>
+                            <option value="date">Date</option>
+                            <option value="mt-total">Montant total</option>
+                        </select>
+                        <select name="croissance">
+                            <option selected disabled hidden>--</option>
+                            <option value="asc">ordre croissant</option>
+                            <option value="desc">ordre décroissant</option>
+                        </select>
+                        <button type="submit">Trier</button>
+                    </form>
+
+
+                    <!-- EXPORT -->
+                    <form class="table-export" onsubmit="return false">
+                        <p>Format du tableau :</p>
+                        <select name="table-format" class="table-format">
+                            <option value="csv">CSV</option>
+                            <option value="pdf">PDF</option>
+                            <option value="xls">XLS</option>
+                        </select>
+                        <button onclik="exporter('po-par-client-display')">Exporter</button>
+                    </form>
+                </div>
             </div>
             
         </section>
@@ -186,6 +295,7 @@
 
     <!-- FOOTER -->
     <footer>
+        <script src="exports.js"></script>
         <script>
             //Option : tous-clients / par-client
             function displayTable(optionId, displayId) {
